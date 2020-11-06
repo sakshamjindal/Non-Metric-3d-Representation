182d6a756ac8f93df77eb88b83950f2f7a09acf1
sakshamjindal <saksham.jindal@outlook.com>
2020-11-06 14:07:34 +0000

    context feature removed from scene grah

--- run.sh
+++ run.sh
@@ -2,4 +2,19 @@ python train.py --batch-size 5 \
                 --seed 0 \
                 --exp-dir single_obj_exp1 \
                 --warmup-epoch 10 \
-                --num-cluster 50
+                --num-cluster 50 \
+                --moco_r 40 \
+                --hyp_N 1 \
+                --mode "node" \
+                --data "/home/mprabhud/dataset/clevr_lang/npys/aa_5t.txt"
+                
+
+python train.py --batch-size 5 \
+                --seed 0 \
+                --exp-dir single_obj_exp1 \
+                --warmup-epoch 1 \
+                --num-cluster 120 \
+                --moco_r 40 \
+                --hyp_N 1 \
+                --mode "node" \
+                --data "/home/mprabhud/dataset/clevr_lang/npys/aa_5t.txt"
