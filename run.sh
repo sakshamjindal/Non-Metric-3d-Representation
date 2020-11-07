2b16bee9b67a7f202a1064fdfc7af21eceed5035
sakshamjindal <saksham.jindal@outlook.com>
2020-11-07 14:51:04 -0500

    run configs changed

--- run.sh
+++ run.sh
@@ -1,32 +1,31 @@
 python train.py --batch-size 5 \
                 --seed 0 \
                 --exp-dir single_obj_exp1 \
-                --warmup-epoch 10 \
+                --warmup-epoch 1 \
                 --num-cluster 120 \
                 --moco_r 40 \
-                --hyp_N 1 \
+                --hyp_N 2 \
                 --mode "node" \
-                --data "/home/mprabhud/dataset/clevr_lang/npys/aa_5t.txt"
-                
-python train.py --batch-size 5 \
-                --seed 0 \
-                --exp-dir two_obj_exp1 \
-                --warmup-epoch 10 \
-                --num-cluster 200 \
-                --moco_r 100 \
-                --hyp_N 2 \
-                --mode "spatial" \
                 --data "/home/mprabhud/dataset/clevr_lang/npys/ab_5t.txt"
-                --use_pretrained 
+                
+# python train.py --batch-size 5 \
+#                 --seed 0 \
+#                 --exp-dir two_obj_exp1 \
+#                 --warmup-epoch 100 \
+#                 --num-cluster 200 \
+#                 --moco_r 100 \
+#                 --hyp_N 2 \
+#                 --mode "spatial" \
+#                 --data "/home/mprabhud/dataset/clevr_lang/npys/ab_5t.txt"
                 
                 
-python train.py --batch-size 5 \
-                --seed 0 \
-                --exp-dir two_obj_exp1 \
-                --warmup-epoch 60 \
-                --num-cluster 200 \
-                --moco_r 100 \
-                --hyp_N 2 \
-                --mode "spatial" \
-                --data "/home/mprabhud/dataset/clevr_lang/npys/ab_5t.txt" \
-                --use_pretrained "tb_logs/single_obj_exp1/checkpoint.pth.tar"
+# python train.py --batch-size 5 \
+#                 --seed 0 \
+#                 --exp-dir two_obj_exp2_pretrained_on_node \
+#                 --warmup-epoch 100 \
+#                 --num-cluster 200 \
+#                 --moco_r 100 \
+#                 --hyp_N 2 \
+#                 --mode "spatial" \
+#                 --data "/home/mprabhud/dataset/clevr_lang/npys/ab_5t.txt" \
+#                 --use_pretrained "tb_logs/single_obj_exp1/checkpoint.pth.tar"
