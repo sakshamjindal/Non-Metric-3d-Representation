e823b9a282f4f2457b12fb5a1629c72e4fdb9894
sakshamjindal <saksham.jindal@outlook.com>
2020-11-08 11:09:33 +0000

    scene and view model combined

--- core/_nbdev.py
+++ core/_nbdev.py
@@ -3,9 +3,7 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"Encoder": "Encoder.ipynb",
-         "MoCo_view": "MoCo-view.ipynb",
-         "MoCo": "MoCo.ipynb",
-         "concat_all_gather": "MoCo.ipynb",
+         "MoCo_scene_and_view": "MoCo-scene_and_view.ipynb",
          "SceneGraph": "Scene-graph.ipynb",
          "setup_tb": "train_model.ipynb",
          "run_training": "train_model.ipynb",
@@ -13,7 +11,6 @@ index = {"Encoder": "Encoder.ipynb",
          "val_retrieval": "train_model.ipynb"}
 
 modules = ["model/encoder.py",
-           "model/model_view.py",
            "model/model.py",
            "model/scene_graph/scene_graph.py",
            "trainer.py"]
